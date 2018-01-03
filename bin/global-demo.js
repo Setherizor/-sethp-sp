6608b9012cd90444f7474983057e76a62fee81a2
Setherizor <setherizor@gmail.com>
2018-01-03 15:59:29 -0500

    added sp2 command

--- bin/global-demo.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-
-var x = require('../lib/index.js');
-x.say('Jack, get back, come on before we crack Lose your blues, everybody cut footloose');
---
+++ bin/sp.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+const x = require('../lib/index.js')
+
+x.say('Jack, get back, come on before we crack Lose your blues, everybody cut footloose')
